90e1946a569ea64b01ac44eaf37755a619137020
Roberto <Roberto@analiseTI.cdlfor.local>
2014-10-09 17:57:45 -0300

    Mapeamento Classe Pessoa

---
+++ src/br/com/sip/model/entity/Pessoa.java
@@ -0,0 +1,47 @@
+package br.com.sip.model.entity;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Roberto
+ */
+@Entity
+@Table(name = "Pessoa")
+
+public class Pessoa implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(name = "ID", nullable = false)
+    private Integer idPessoa;
+
+    @Column(name = "TIPOPESSOA", length = 1, nullable = false)
+    private String TipoPessoa;
+
+    @Column(name = "CNPJCPF", length = 14, nullable = false, unique = true)
+    private String CNPJCPF;
+
+    @Column(name = "NOME_RAZAO", length = 100, nullable = false)
+    private String NomeRazao;
+
+    @Column(name = "FANTASIA", length = 100, nullable = false)
+    private String Fantasia;
+
+    @Column(name = "SITUACAO", length = 1, nullable = false)
+    private String Situacao;
+    
+    @Column(name = "ORIGEM", length = 20, nullable = false)
+    private String Origem;
+    
+    
+}
