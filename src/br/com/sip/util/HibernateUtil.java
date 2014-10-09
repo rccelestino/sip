4a07412c95bbe217a7371aa62a8374a887b1607d
Roberto <Roberto@analiseTI.cdlfor.local>
2014-10-09 17:16:27 -0300

    Fase Inicial do Projeto

--- src/br/com/sip/util/HibernateUtil.java
+++ src/br/com/sip/util/HibernateUtil.java
@@ -1,7 +1,9 @@
 package br.com.sip.util;
 
-import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 
 /**
  * Hibernate Utility class with a convenient method to get Session Factory
@@ -12,19 +14,24 @@ import org.hibernate.SessionFactory;
 public class HibernateUtil {
 
     private static final SessionFactory sessionFactory;
-    
+    public static final String HIBERNATE_SESSION = "hibernate_session";
+
     static {
         try {
-            // Create the SessionFactory from standard (hibernate.cfg.xml) 
-            // config file.
-            sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
+            System.out.println("Tentando criar Session Factory");
+            Configuration cfg = new Configuration().configure();
+            ServiceRegistry srv = new StandardServiceRegistryBuilder().applySettings(
+            cfg .getProperties()).build();
+            
+            sessionFactory = cfg.buildSessionFactory(srv);
+            System.out.println("Session Factory criada com sucesso");
+
         } catch (Throwable ex) {
-            // Log the exception. 
-            System.err.println("Initial SessionFactory creation failed." + ex);
+            System.err.println("Falha na inicialização da Session Factory." + ex);
             throw new ExceptionInInitializerError(ex);
         }
     }
-    
+
     public static SessionFactory getSessionFactory() {
         return sessionFactory;
     }
